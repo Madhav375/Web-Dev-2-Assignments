0654024a2abc9717095a7b667b86e3f4c8c31262
Madhav375 <madhavkalra375@gmail.com>
2026-02-08 13:50:15 -0500

    Made it a little working it now shows the details on the scrfeen but do not save it

---
+++ script.js
@@ -0,0 +1,6 @@
+let form = document.getElementById("eventForm");
+let eventList = document.getElementById("eventList");
+
+let clearBtn = document.getElementById("clearBtn");
+let sampleBtn = document.getElementById("sampleBtn");
+
