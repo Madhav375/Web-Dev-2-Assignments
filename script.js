089f4a5e582de30c271848526ea801940766a487
Madhav375 <madhavkalra375@gmail.com>
2026-02-08 14:20:55 -0500

    Applied some more advanced type JS in the smart dashboard and made it a little more working

--- script.js
+++ script.js
@@ -4,3 +4,28 @@ let eventList = document.getElementById("eventList");
 let clearBtn = document.getElementById("clearBtn");
 let sampleBtn = document.getElementById("sampleBtn");
 
+form.addEventListener("submit", function(e) {
+
+    e.preventDefault();
+
+    let eventName = document.getElementById("name").value;
+    let eventDate = document.getElementById("date").value;
+    let eventType = document.getElementById("type").value;
+    let eventDesc = document.getElementById("desc").value;
+
+    let card = document.createElement("div");
+    card.className = "eventCard";
+
+    card.innerHTML =
+        "<h4>" + eventName + "</h4>" +
+        "<p>Date: " + eventDate + "</p>" +
+        "<p>Type: " + eventType + "</p>" +
+        "<p>" + eventDesc + "</p>" +
+        "<button class='deleteBtn'>Delete</button>";
+
+    eventList.appendChild(card);
+
+    form.reset();
+});
+
+
